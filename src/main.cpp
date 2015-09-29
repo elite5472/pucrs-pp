7d89e6431340e920d77695dfc796a2a5e8d66142
Guillermo Borges <guiborges@live.com>
2015-09-29 01:13:23 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,30 @@
-#include "mpi.h"
+//#include "mpi.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
-int main(int argc, char* argv []){
+using namespace std;
 
-int rank,numprocess;
-
-MPI_Init( &argc, &argv );
-MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-MPI_Comm_size( MPI_COMM_WORLD, &numprocess );
-
-
-if(rank==0){ //master
-
-    MPI_Status s;int Sstate=1;int Rstate;
-
-    int p;
-    for(p=1;p<numprocess;p++){
-
-        fflush(stdout);printf("master : order P%d to start reading\n",p);
-        MPI_Send(&Sstate, sizeof(int), MPI_INT, p, 20, MPI_COMM_WORLD);
-
-        MPI_Recv(&Rstate,sizeof(int),MPI_INT,p,21,MPI_COMM_WORLD,&s);
-        fflush(stdout);printf("master : P%d finished reading\n",p);
-    }
-
+int compare_numbers(const void* x, const void* y{
+	return (*(int*)x - *(int*)y);
 }
-else{ //workers
 
-    int state; MPI_Status s;
-    MPI_Recv(&state,sizeof(int),MPI_INT,0,20,MPI_COMM_WORLD,&s);
-
-    //send to master : finish reading
-    state=2;
-    MPI_Send(&state, sizeof(int), MPI_INT, 0, 21, MPI_COMM_WORLD);
+void master()
+{
     
-    fflush(stdout);printf("worker %d ended processing\n",rank);
 }
 
-MPI_Finalize();
-return 0;
+void slave()
+{
+    
+}
+
+int main(int argc, char* argv [])
+{
+
+int rank;
+int processes = atoi(argv[1]);
+
+cout << processes;
+
 }
