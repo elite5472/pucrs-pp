30885a07725a48ada095fbd0a56f233458b31ccc
Guillermo Borges <guiborges@live.com>
2015-12-01 09:31:56 -0200

    updated .sh file

--- divcon.sh
+++ divcon.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-ladrun -np 2 bin/tp3omp 1 10000 1000
-ladrun -np 2 bin/tp3omp 2 10000 1000
-ladrun -np 2 bin/tp3omp 4 10000 1000
-ladrun -np 2 bin/tp3omp 8 10000 1000
-ladrun -np 2 bin/tp3omp 16 10000 1000
-ladrun -np 2 bin/tp3omp 32 10000 1000
+ladrun -np 2 bin/tp3omp 1 10000 10000
+ladrun -np 2 bin/tp3omp 2 10000 10000
+ladrun -np 2 bin/tp3omp 4 10000 10000
+ladrun -np 2 bin/tp3omp 8 10000 10000
+ladrun -np 2 bin/tp3omp 16 10000 10000
+ladrun -np 2 bin/tp3omp 32 10000 10000
 
-ladrun -np 4 bin/tp3omp 1 10000 1000
-ladrun -np 4 bin/tp3omp 2 10000 1000
-ladrun -np 4 bin/tp3omp 4 10000 1000
-ladrun -np 4 bin/tp3omp 8 10000 1000
-ladrun -np 4 bin/tp3omp 16 10000 1000
-ladrun -np 4 bin/tp3omp 32 10000 1000
+ladrun -np 4 bin/tp3omp 1 10000 10000
+ladrun -np 4 bin/tp3omp 2 10000 10000
+ladrun -np 4 bin/tp3omp 4 10000 10000
+ladrun -np 4 bin/tp3omp 8 10000 10000
+ladrun -np 4 bin/tp3omp 16 10000 10000
+ladrun -np 4 bin/tp3omp 32 10000 10000
 
-ladrun -np 8 bin/tp3omp 1 10000 1000
-ladrun -np 8 bin/tp3omp 2 10000 1000
-ladrun -np 8 bin/tp3omp 4 10000 1000
-ladrun -np 8 bin/tp3omp 8 10000 1000
-ladrun -np 8 bin/tp3omp 16 10000 1000
-ladrun -np 8 bin/tp3omp 32 10000 1000
+ladrun -np 8 bin/tp3omp 1 10000 10000
+ladrun -np 8 bin/tp3omp 2 10000 10000
+ladrun -np 8 bin/tp3omp 4 10000 10000
+ladrun -np 8 bin/tp3omp 8 10000 10000
+ladrun -np 8 bin/tp3omp 16 10000 10000
+ladrun -np 8 bin/tp3omp 32 10000 10000
 
