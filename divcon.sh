e76fe8ba1687ab59830ddcd637afeadc7f2495f2
Guillermo Borges <guiborges@live.com>
2015-12-01 10:06:40 -0200

    update

--- divcon.sh
+++ divcon.sh
@@ -5,19 +5,15 @@ ladrun -np 2 bin/tp3omp 2 10000 50000
 ladrun -np 2 bin/tp3omp 4 10000 50000
 ladrun -np 2 bin/tp3omp 8 10000 50000
 ladrun -np 2 bin/tp3omp 16 10000 50000
-ladrun -np 2 bin/tp3omp 32 10000 50000
+#ladrun -np 2 bin/tp3omp 32 10000 50000
+
+ladrun -np 2 bin/tp3omp 1 10000 50000
+ladrun -np 4 bin/tp3omp 1 10000 50000
+ladrun -np 8 bin/tp3omp 1 10000 5000
 
 ladrun -np 4 bin/tp3omp 1 10000 50000
 ladrun -np 4 bin/tp3omp 2 10000 50000
 ladrun -np 4 bin/tp3omp 4 10000 50000
 ladrun -np 4 bin/tp3omp 8 10000 50000
 ladrun -np 4 bin/tp3omp 16 10000 50000
-ladrun -np 4 bin/tp3omp 32 10000 50000
-
-ladrun -np 8 bin/tp3omp 1 10000 50000
-ladrun -np 8 bin/tp3omp 2 10000 50000
-ladrun -np 8 bin/tp3omp 4 10000 50000
-ladrun -np 8 bin/tp3omp 8 10000 50000
-ladrun -np 8 bin/tp3omp 16 10000 50000
-ladrun -np 8 bin/tp3omp 32 10000 50000
-
+#ladrun -np 4 bin/tp3omp 32 10000 50000
